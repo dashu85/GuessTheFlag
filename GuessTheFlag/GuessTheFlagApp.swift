9b648b71dbbd0eb315bcfccc9c03312e608f1a30
Marcus Benoit <benoit.marcus@gmail.com>
2024-03-11 16:25:19 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marcus Benoit on 11.03.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
